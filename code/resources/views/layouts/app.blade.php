138f60d9b698663ee3f12bd7f7942d8a4919339b
VadimSnytko <inhellimaybo@gmail.com>
2020-04-08 00:40:43 +0600

    change title

--- code/resources/views/layouts/app.blade.php
+++ code/resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Community updates</title>
 </head>
 <body>
 <div class="content-wrapper">
