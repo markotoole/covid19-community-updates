381ad4875233a7efecaf09337ba5ecac57c56e99
VadimSnytko <inhellimaybo@gmail.com>
2020-04-10 21:49:23 +0600

    fix table blinking

--- code/resources/views/layouts/app.blade.php
+++ code/resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
     </div>
     <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
         <div class="container text-center">
-            <small>Idea & Design by Ross McCarthy (<a href="https://status-naas.com">status-naas.com</a>). Developed by CiviQ. Contact info@civiq.eu | 086 1408681</small>
+            <small>Idea & Design by Ross McCarthy (<a href="https://www.status-naas.com/">status-naas.com</a>). Developed by CiviQ. Contact info@civiq.eu | 086 1408681</small>
         </div>
     </footer>
 </div>
