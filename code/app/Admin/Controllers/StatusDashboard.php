159ef11ad2e86263509113561ebcff65240421e0
VadimSnytko <inhellimaybo@gmail.com>
2020-04-21 16:47:13 +0600

    added permissions

--- code/app/Admin/Controllers/StatusDashboard.php
+++ code/app/Admin/Controllers/StatusDashboard.php
@@ -3,17 +3,26 @@
 namespace App\Admin\Controllers;
 
 use App\Models\UpdateRequest;
+use Encore\Admin\Facades\Admin;
 
 class StatusDashboard
 {
     public static function operations()
     {
         $newRequests = UpdateRequest::where('state', '=', 'new')
-                                     ->count();
-        $links = [
-            ['name' => 'Public services', 'link' => '/admin/service-statuses?state=public'],
-            ['name' => sprintf("New requests (%s)", $newRequests), 'link' => '/admin/update-requests?&status=new'],
-        ];
+                                    ->count();
+
+        if ($showServices = Admin::user()
+                                 ->isRole('administrator')) {
+            $links = [
+                ['name' => 'Public services', 'link' => '/admin/service-statuses?state=public'],
+            ];
+        } else {
+            $links = [];
+        }
+
+        $links[] =
+            ['name' => sprintf("New requests (%s)", $newRequests), 'link' => '/admin/update-requests?&status=new'];
 
         return view('admin/box', compact('links'));
     }
