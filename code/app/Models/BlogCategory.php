d553b91979cfd06130cc7782a67d7de73af6eb7e
VadimSnytko <inhellimaybo@gmail.com>
2020-04-27 13:46:04 +0600

    update email

--- code/app/Models/BlogCategory.php
+++ code/app/Models/BlogCategory.php
@@ -10,12 +10,12 @@ class BlogCategory
             'updates' => [
                 'title' => 'Council updates',
                 'description' => 'Please let us know of other updates by emailing <a
-                        href="mailto:hello@ournewbridge.info">hello@ournewbridge.info</a>',
+                        href="mailto:hello@ournewbridge@gmail.com">hello@ournewbridge@gmail.com</a>',
             ],
             'supports' => [
                 'title' => 'Business & Community supports',
                 'description' => 'Please let us know if there are other supports that should be included by emailing <a
-                        href="mailto:hello@ournewbridge.info">hello@ournewbridge.info</a>',
+                        href="mailto:hello@ournewbridge@gmail.com">hello@ournewbridge@gmail.com</a>',
             ],
         ];
 
